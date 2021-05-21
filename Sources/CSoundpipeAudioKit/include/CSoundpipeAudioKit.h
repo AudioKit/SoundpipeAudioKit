76a3272c849f9d2d3e52e2d82f4e2dd3fab724b3
Aurelius Prochazka <aurephd@gmail.com>
2021-05-21 05:24:18 -0700

    Added public interface

---
+++ Sources/CSoundpipeAudioKit/include/CSoundpipeAudioKit.h
@@ -0,0 +1,8 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+#ifndef CSoundpipeAudioKit_h
+#define CSoundpipeAudioKit_h
+
+// CSoundpipeAudioKit has no public interface. It registers DSP kernels automatically.
+
+#endif /* CSoundpipeAudioKit_h */
