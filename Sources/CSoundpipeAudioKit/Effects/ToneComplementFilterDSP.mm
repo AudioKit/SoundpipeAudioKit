4845e31bbd36fd7cb4f640d0a5fc2a5bc04d6b18
Aurelius Prochazka <aurephd@gmail.com>
2021-05-24 13:48:10 -0700

    Updated out of date headers

--- Sources/CSoundpipeAudioKit/Effects/ToneComplementFilterDSP.mm
+++ Sources/CSoundpipeAudioKit/Effects/ToneComplementFilterDSP.mm
@@ -1,4 +1,4 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+// Copyright AudioKit. All Rights Reserved.
 
 #include "SoundpipeDSPBase.h"
 #include "ParameterRamper.h"
