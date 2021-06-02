5f28f95c629ebb20252153d35ff1a2ca9cbee2b6
Aurelius Prochazka <aurephd@gmail.com>
2021-06-01 21:56:15 -0700

    Updated README

--- Package.swift
+++ Package.swift
@@ -12,8 +12,6 @@ let package = Package(
         .package(url: "https://github.com/AudioKit/AudioKit", .branch("develop")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "Soundpipe",
                 dependencies: ["KissFFT"],
                 exclude: ["lib/inih/LICENSE.txt"],
