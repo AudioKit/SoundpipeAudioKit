e29296f426489e2299cd50318847c340dca1d802
Aurelius Prochazka <aurephd@gmail.com>
2021-06-04 23:20:37 -0700

    Removed hacky fix

--- Playgrounds/Hello World.playground/Contents.swift
+++ Playgrounds/Hello World.playground/Contents.swift
@@ -12,8 +12,5 @@ try! engine.start()
 
 while true {
     osc.frequency = Float.random(in: 200...800)
-    osc.amplitude = 0.3
-    usleep(10000)
-    osc.$amplitude.ramp(to: 0.0, duration: 0.9)
     sleep(1)
 }
