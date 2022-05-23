c11266dc6f6a94cf76a308cf5a0ea2a54a74ac7b
Josip Cavar <josipcavar04@gmail.com>
2022-05-23 23:30:26 +0200

    Fix a memory leak in PitchShifter reset

---
+++ Tests/SoundpipeAudioKitTests/Effects Tests/PitchShifterTests.swift
@@ -0,0 +1,24 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import AudioKit
+import SoundpipeAudioKit
+import XCTest
+
+class PitchShifterTests: XCTestCase {
+
+    func testResetPitchShifter() {
+        let engine = AudioEngine()
+        let input = Oscillator(waveform: Table([1, 1]))
+        let shifter = PitchShifter(input)
+        engine.output = shifter
+        input.start()
+        let audio = engine.startTest(totalDuration: 2.0)
+        audio.append(engine.render(duration: 1.0))
+        XCTAssertEqual(audio.toFloatChannelData()?[0].count, 44100)
+
+        shifter.reset()
+        audio.append(engine.render(duration: 1.0))
+
+        XCTAssertEqual(audio.toFloatChannelData()?[0].count, 88200)
+    }
+}
