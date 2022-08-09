3496a218d65d4676bd2950566a7d976cffed0be5
James Pike <james.pike@lisnaudio.com>
2022-08-09 12:39:13 -0600

    Added HRIR, obtained from 'LISTEN HRTF DATABASE'

--- Tests/SoundpipeAudioKitTests/Effects Tests/ConvolutionTests.swift
+++ Tests/SoundpipeAudioKitTests/Effects Tests/ConvolutionTests.swift
@@ -21,11 +21,8 @@ class ConvolutionTests: XCTestCase {
         let convolution = Convolution(player,
                                       impulseResponseFileURL: dishURL,
                                       partitionLength: 8_192)
-        
 
         engine.output = convolution
-        
-        
         
         let audio = engine.startTest(totalDuration: 2.0)
         player.file = file
@@ -35,6 +32,35 @@ class ConvolutionTests: XCTestCase {
         
         testMD5(audio)
 
+    }
+    
+    
+    func testStereoConvolution() {
+        guard let url = Bundle.module.url(forResource: "TestResources/drumloop", withExtension: "wav"),
+              let file = try? AVAudioFile(forReading: url) else {
+            XCTFail("Didn't generate test file")
+            return
+        }
+        
+        let engine = AudioEngine()
+        let player = AudioPlayer()
+        
+        let dishURL = Bundle.module.url(forResource: "TestResources/dish", withExtension: "wav")!
+        let convolution = Convolution(player,
+                                      impulseResponseFileURL: dishURL,
+                                      partitionLength: 8_192)
+
+        engine.output = convolution
+        
+        let audio = engine.startTest(totalDuration: 2.0)
+        player.file = file
+        
+        player.play()
+        audio.append(engine.render(duration: 2.0))
+        
+        testMD5(audio)
+        
+        
     }
 }
 
